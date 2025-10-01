8062195d7fce026a47a3ed3664928eed8d80a012
pedrosier <pedrosier001@gmail.com>
2025-10-01 10:51:40 -0300

    DesafioClassHero002
    Resolução 002 ClassHero

---
+++ ClassHero002/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite sua idade:");
+        int idade = int.Parse(Console.ReadLine());
+
+        if (idade < 12)
+        {
+            Console.WriteLine("Criança");
+        }
+        else if (idade >= 12 && idade <= 17)
+        {
+            Console.WriteLine("Adolescente");
+        }
+        else
+        {
+            Console.WriteLine("Adulto");
+        }
+    }
+}
